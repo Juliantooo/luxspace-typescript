85fc17e43dc9dfb2ae867d70a9a630714e77a263
Fajar Julianto <slashcrackdie@gmail.com>
2022-03-26 16:56:25 +0700

    implement layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import MainLayout from '../layouts/mainLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </ChakraProvider>
   )
 }
